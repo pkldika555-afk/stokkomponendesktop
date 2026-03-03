b1dbcf8dc14e8bc9e5d50383af7c47da4229d7a6
dikapkl <dikapkl555@example.com>
2026-03-03 09:05:48 +0700

    feat: change nomor to harga

--- resources/views/komponen/edit.blade.php
+++ resources/views/komponen/edit.blade.php
@@ -188,7 +188,7 @@
                         <div class="relative">
                             <input type="number" id="harga" name="harga" value="{{ old('harga', $komponen->harga) }}" min="0"
                                 class="w-full bg-gray-800 border {{ $errors->has('harga') ? 'border-rose-500' : 'border-gray-700' }} text-gray-100 rounded-xl px-4 py-3 pr-16 text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent transition font-mono">
-                            <span class="absolute right-4 top-1/2 -translate-y-1/2 text-xs text-gray-500">nomor</span>
+                            <span class="absolute right-4 top-1/2 -translate-y-1/2 text-xs text-gray-500">harga</span>
                         </div>
                         @error('harga')
                             <p class="mt-1.5 text-xs text-rose-400">{{ $message }}</p>
