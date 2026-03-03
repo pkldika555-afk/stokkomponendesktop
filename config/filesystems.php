89a3f1ddd9b816a8b9a836d3fc69b895a4f2e48d
dikapkl <dikapkl555@example.com>
2026-03-03 12:54:22 +0700

    add new path

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,6 +55,11 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
+        'app_data_images' => [
+            'driver' => 'local',
+            'root' => storage_path('app_data/images'),
+            'visibility' => 'public',
+        ],
 
     ],
 
