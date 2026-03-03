69db958264e5d8f8edfc037fdbe61d3773e7837c
dikapkl <dikapkl555@example.com>
2026-03-03 10:54:27 +0700

    feat: costum paginator design

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\MutasiBarang;
 use App\Observers\MutasiBarangObserver;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,5 +27,6 @@ class AppServiceProvider extends ServiceProvider
         \Illuminate\Support\Facades\Validator::extend('nrp', function ($attribute, $value, $parameters, $validator) {
             return preg_match('/^[0-9]{6}$/', $value);
         }, 'NRP harus terdiri dari 6 digit angka.');
+        Paginator::defaultView('vendor.pagination.default');
     }
 }
